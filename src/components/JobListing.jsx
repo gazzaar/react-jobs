c51a0e751eff92036980b2a04a82ead97a8017d4
fathyElgazar <gazzarDev@gmail.com>
2024-06-07 08:07:51 +0300

    Working with react icons packages

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 const JobListing = ({ job }) => {
   const [showFullDescription, setShowFullDescription] = useState(false);
   let description = job.description;
@@ -17,8 +18,8 @@ const JobListing = ({ job }) => {
         <div className="mb-5">{description}</div>
         <button
           onClick={() => setShowFullDescription((prevState) => !prevState)}
-          className="text-indigo-500 mb-5"
-          hover:text-indigo-600
+          className="text-indigo-500 mb-5 hover:text-indigo-600
+"
         >
           {showFullDescription ? "Less" : "More"}
         </button>
@@ -28,7 +29,7 @@ const JobListing = ({ job }) => {
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {job.location}
           </div>
           <a
