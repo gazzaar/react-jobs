774c7f7c71738356f569de6e2317403973cc3843
fathyElgazar <gazzarDev@gmail.com>
2024-06-23 15:46:56 +0300

    Fixing a typo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = ({
   title = "Become a React Dev",
-  subtitle = "Find the React jog that fits your skills and needs",
+  subtitle = "Find the React job that fits your skills and needs",
 }) => {
   return (
     <section className="bg-indigo-700 py-20 mb-4">
