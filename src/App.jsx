bb5a014c1b9bc69f8716c0de7deaf83bc4d26fbf
fathyElgazar <gazzarDev@gmail.com>
2024-06-26 18:11:35 +0300

    working with data loader

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,22 @@ import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
-} from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import MainLayout from './layouts/MainLayout';
-import JobsPage from './pages/JobsPage';
-import NotFoundPage from './pages/NotFoundPage';
+} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import MainLayout from "./layouts/MainLayout";
+import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import JobPage, { jobLoader } from "./pages/JobPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path="/jobs" element={<JobsPage />} />
+      <Route path="/jobs/:id" element={<JobPage />} loader={jobLoader} />
       <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  )
+    </Route>,
+  ),
 );
 
 const App = () => {
