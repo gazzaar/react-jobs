dc8b5f837e50364f48b8720f9af04e8b3be80ec7
fathyElgazar <gazzarDev@gmail.com>
2024-06-27 12:44:07 +0300

    Add toastify for add and delete job

--- src/pages/AddJobPage.jsx
+++ src/pages/AddJobPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const AddJobPage = ({ addJobSubmit }) => {
   const [title, setTitle] = useState("");
@@ -28,6 +29,7 @@ const AddJobPage = ({ addJobSubmit }) => {
       },
     };
     addJobSubmit(newJob);
+    toast.success("Job Added successfully");
     return navigate("/jobs");
   };
 
